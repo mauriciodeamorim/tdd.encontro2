405f10bd5ce5e51556030c547259c8c5723b7d84
Mauricio de Amorim <amorim@timedev.com>
2009-01-08 00:10:46 -0200

    First commit to upload project

---
+++ Tdd.Encontro2.Test/HoraTest.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tdd.Encontro2;
+
+namespace Tdd.Encontro2.Test
+{
+    /// <summary>
+    /// Summary description for HoraTest
+    /// </summary>
+    [TestClass]
+    public class HoraTest
+    {
+        public HoraTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Criacao_Hora_Informando_Hora_E_Minuto()
+        {
+            Hora hora = new Hora("10:30");
+
+            Assert.AreEqual<string>("10:30", hora.ToString());
+        }
+
+        [TestMethod]
+        public void Criacao_Hora_Nao_Informando_Hora_E_Minuto()
+        {
+            Hora hora = new Hora();
+
+            Assert.AreEqual<string>("00:00", hora.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Criacao_Hora_Formato_Invalido_Excecao_Esperada()
+        {
+            Hora hora = new Hora("00-00");
+        }
+
+        [TestMethod]
+        public void Criacao_Hora_Formato_Invalido()
+        {
+            try
+            {
+                Hora hora = new Hora("00-00");
+                Assert.Fail("O formato '00-00' é inválido. Uma ArgumentException deveria ser lançada.");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual<string>("Formato de Hora inválido: 00-00", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void Criacao_Hora_Com_Um_Digito()
+        {
+            Hora hora = new Hora("9:30");
+
+            Assert.AreEqual<string>("9:30", hora.ToString());
+        }
+
+        [TestMethod]
+        public void Criacao_Hora_Com_Mais_De_Dois_Digitos()
+        {
+            Hora hora = new Hora("130:00");
+
+            Assert.AreEqual<string>("130:00", hora.ToString());
+        }
+
+        [TestMethod]
+        public void Criacao_Hora_Sem_Digito_De_Horas()
+        {
+            try
+            {
+                Hora hora = new Hora(":15");
+                Assert.Fail("Uma hora não pode ser criada só com minutos. Uma ArgumentException deveria ser lançada.");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual<string>("Formato de Hora inválido: :15", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void Criacao_Hora_Negativa()
+        {
+            Hora hora = new Hora("-1:00");
+
+            Assert.AreEqual<string>("-1:00", hora.ToString());
+        }
+
+        [TestMethod]
+        public void Criacao_Hora_Com_Minutos_Invalidos()
+        { 
+            try
+            {
+                Hora hora = new Hora("5:60");
+                Assert.Fail("Os minutos de '5:60' são inválidos. Uma ArgumentException deveria ser lançada.");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual<string>("Formato de Hora inválido: 5:60", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void Soma_Horas_Com_Um_Digito()
+        {
+            Hora hora = new Hora("3:00");
+            hora.SomaHoras("5:00");
+
+            Assert.AreEqual<string>("8:00", hora.ToString());
+        }
+
+        [TestMethod]
+        public void Soma_Horas_Com_Mais_De_Um_Digito()
+        {
+            Hora hora = new Hora("1234:00");
+            hora.SomaHoras("6:00");
+
+            Assert.AreEqual<string>("1240:00", hora.ToString());        
+        }
+
+        [TestMethod]
+        public void Soma_Horas_Com_Valor_Negativo()
+        {
+            Hora hora = new Hora("333:00");
+            hora.SomaHoras("-3:00");
+
+            Assert.AreEqual<string>("330:00", hora.ToString());
+        }
+
+        [TestMethod]
+        public void Soma_Horas_Com_Minutos()
+        {
+            Hora hora = new Hora("1:10");
+            hora.SomaHoras("2:15");
+
+            Assert.AreEqual<string>("3:25", hora.ToString());
+        }
+
+        [TestMethod]
+        public void Soma_Horas_Formato_Invalido()
+        {
+            try
+            {
+                Hora hora = new Hora("1:10");
+                hora.SomaHoras("2-15");
+
+                Assert.Fail("O formato '2-15' é inválido. Uma ArgumentException deveria ser lançada.");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual<string>("Formato de Hora inválido: 2-15", e.Message);
+            }
+        }
+
+        [TestMethod]
+        [DeploymentItem("Tdd.Encontro2.dll")]
+        public void Validar_Hora_Informando_Hora_E_Minuto()
+        {
+            Hora_Accessor hora = new Hora_Accessor();
+            hora.ValidarHora("10:30");
+        }
+
+        [TestMethod]
+        [DeploymentItem("Tdd.Encontro2.dll")]
+        public void Validar_Hora_Accessor_Formato_Invalido()
+        {
+            try
+            {
+                Hora_Accessor hora = new Hora_Accessor();
+                hora.ValidarHora("00-00");
+
+                Assert.Fail("O formato '00-00' é inválido. Uma ArgumentException deveria ser lançada.");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual<string>("Formato de Hora inválido: 00-00", e.Message);
+            }
+        }
+
+        [TestMethod]
+        [DeploymentItem("Tdd.Encontro2.dll")]
+        public void Validar_Hora_Accessor_Com_Um_Digito()
+        {
+            Hora_Accessor hora = new Hora_Accessor();
+            hora.ValidarHora("9:30");
+        }
+
+        [TestMethod]
+        [DeploymentItem("Tdd.Encontro2.dll")]
+        public void Validar_Hora_Accessor_Com_Mais_De_Dois_Digitos()
+        {
+            Hora_Accessor hora = new Hora_Accessor();
+            hora.ValidarHora("130:00");
+        }
+
+        [TestMethod]
+        [DeploymentItem("Tdd.Encontro2.dll")]
+        public void Validar_Hora_Accessor_Sem_Digito_De_Hora_Accessors()
+        {
+            try
+            {
+                Hora_Accessor hora = new Hora_Accessor();
+                hora.ValidarHora(":15");
+
+                Assert.Fail("Uma hora não pode ser criada só com minutos. Uma ArgumentException deveria ser lançada.");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual<string>("Formato de Hora inválido: :15", e.Message);
+            }
+        }
+
+        [TestMethod]
+        [DeploymentItem("Tdd.Encontro2.dll")]
+        public void Validar_Hora_Accessor_Negativa()
+        {
+            Hora_Accessor hora = new Hora_Accessor();
+            hora.ValidarHora("-1:00");
+        }
+
+        [TestMethod]
+        [DeploymentItem("Tdd.Encontro2.dll")]
+        public void Validar_Hora_Accessor_Com_Minutos_Invalidos()
+        {
+            try
+            {
+                Hora_Accessor hora = new Hora_Accessor();
+                hora.ValidarHora("5:60");
+
+                Assert.Fail("Os minutos de '5:60' são inválidos. Uma ArgumentException deveria ser lançada.");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual<string>("Formato de Hora inválido: 5:60", e.Message);
+            }
+        }
+
+        //TODO: Next step - To add one hour in Hora when the minutes are greaters that 59
+        //[TestMethod]
+        //public void Soma_Horas_Com_Minutos_Acima_De_59_Minutos()
+        //{
+        //    Hora hora = new Hora("1:50");
+        //    hora.SomaHoras("2:30");
+
+        //    Assert.AreEqual<string>("4:20", hora.ToString());
+        //}
+        //TODO: Criar testes de ValidarHora passando nulo e "vazio".
+    }
+}
---
+++ Tdd.Encontro2/Hora.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Tdd.Encontro2
+{
+    public class Hora
+    {
+        private string hora;
+
+        public Hora()
+        {
+            this.hora = "00:00";
+        }
+
+        public Hora(string hora)
+        {
+            this.ValidarHora(hora);
+
+            this.hora = hora;
+        }
+
+        public override string ToString()
+        {
+            return this.hora;
+        }
+
+        public void SomaHoras(string hora)
+        {
+            this.ValidarHora(hora);
+
+            string[] horaAtual = this.hora.Split(':');
+            string[] horaAdicional = hora.Split(':');
+
+            int somenteHoras = Convert.ToInt32(horaAtual[0]);
+            int somenteHorasAdicionais = Convert.ToInt32(horaAdicional[0]);
+
+            int somenteMinutos = int.Parse(horaAtual[1]);
+            int somenteMinutosAdicionais = int.Parse(horaAdicional[1]);
+
+            DateTime dt = new DateTime();
+            TimeSpan ts = new TimeSpan();
+
+            int somaHoras = somenteHoras + somenteHorasAdicionais;
+            int somaMinutos = somenteMinutos + somenteMinutosAdicionais;
+
+            this.hora = somaHoras.ToString() + ":" + somaMinutos.ToString().PadLeft(2, '0');
+        }
+
+        private void ValidarHora(string hora)
+        {
+            if (!Regex.IsMatch(hora, "[0-9][0-9]*:[0-5][0-9]"))
+            {
+                throw (new ArgumentException("Formato de Hora inválido: " + hora));
+            }
+        }
+    }
+}
